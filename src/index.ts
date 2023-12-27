10d5bfc06f00c59a3215e93bcf134b331f0410e0
Andrii-Vovk <andrii.vovk0@gmail.com>
2023-12-28 00:35:42 +0200

    feat: add some packages

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
-// src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import morgan from "morgan";
+import cors from "cors";
+
+import pingRouter from "./routes/ping";
 
 dotenv.config();
-
-mongoose.connect(process.env.MONGO_URL || "");
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+  })
+);
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+mongoose.connect(process.env.MONGO_URL || "");
+
+app.use("/ping", pingRouter);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
---
+++ src/routes/ping.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import PingController from "../controllers/ping";
+
+const router = express.Router();
+
+router.get("/", async (_req, res) => {
+  const controller = new PingController();
+  const response = await controller.getMessage();
+  return res.json(response);
+});
+
+export default router;
